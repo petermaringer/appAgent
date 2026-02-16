699fbaccfea5f4e3ca1aca8562e0ff4bb5396cf8
petermaringer <petermaringer@notmailinator.com>
2026-02-16 19:42:29 +0100

    G5

--- appAgent/Package.swift
+++ appAgent/Package.swift
@@ -2,15 +2,30 @@
 import PackageDescription
 
 let package = Package(
-  name: "appAgent",
-  platforms: [.iOS(.v17)],
-  products: [
-    .executable(name: "appAgent", targets: ["appAgent"])
-  ],
-  targets: [
-    .executableTarget(
-      name: "appAgent",
-      dependencies: []
-    )
-  ]
+    name: "appAgent",
+    defaultLocalization: "en",
+    platforms: [
+        .iOS(.v16)
+    ],
+    products: [
+        // Das Modul, das die App bereitstellt
+        .executable(
+            name: "appAgent",
+            targets: ["appAgent"]
+        )
+    ],
+    dependencies: [
+        // Optional: hier könntest du weitere Swift-Pakete einbinden, z.B. für erweitertes Syntax-Highlighting
+        // .package(url: "https://github.com/username/PackageName.git", from: "1.0.0"),
+    ],
+    targets: [
+        .executableTarget(
+            name: "appAgent",
+            dependencies: [],
+            path: "Sources",
+            resources: [
+                .process("Resources")
+            ]
+        )
+    ]
 )
